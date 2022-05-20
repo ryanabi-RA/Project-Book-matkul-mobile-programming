cae2c9c993937af33ff5c81bd7b75d3e476beec5
ryanabi-RA <ryanabi9@gmail.com>
2022-05-21 02:23:57 +0700

    add KategoriUbahScreen js

---
+++ screen/KategoriUbahScreen.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
+import { Appbar, TextInput, Button, Switch, List } from 'react-native-paper';
+import supabase from '../supabase';
+
+function KategoriUbahScreen({navigation, route}) {
+  const [nama, setNama] = useState('');
+  const [penerbit, setPenerbit] = useState('');
+  const [status, setStatus] = useState('');
+  
+  //script dijalankan ketika screen diakses
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async() => {
+    const { data, error } = await supabase
+                              .from('kategori')
+                              .select('nama', 'penerbit', 'status')
+                              .eq('id', route.params.id)
+                              .single();
+    setNama(data.nama);
+    setPenerbit(data.Penerbit);
+    setStatus(data.status);
+  }
+
+  const onSimpan = async() => {
+    const { data, error } = await supabase
+                              .from('kategori')
+                              .update({
+                                nama: nama,
+                                penerbit: penerbit,
+                                status: status,
+                              })
+                              .eq('id', route.params.id);
+
+    Alert.alert("Pesan", "Data berhasil disimpan");
+    navigation.goBack()
+  }
+
+  const konfirmasiHapus = () => {
+    Alert.alert("Pesan", "Yakin hapus data?",
+      [
+        { text: "Batal" },
+        { text: "Hapus", onPress: () => onHapus() }
+      ]
+    );
+  }
+
+  const onHapus = async() => {
+    const { data, error } = await supabase
+                              .from('kategori')
+                              .delete()
+                              .eq('id', route.params.id);
+
+    // Alert.alert("Pesan", "Data berhasil dihapus");
+    navigation.goBack()
+  }
+
+  return (
+    <>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+        <Appbar.Content title="Ubah Kategori" />
+        <Appbar.Action icon="delete" onPress={() => onHapus()} />
+      </Appbar.Header>
+
+      <TextInput
+        label="Nama"
+        value={nama}
+        onChangeText={text => setNama(text)}
+      />
+
+      <TextInput
+        label="Penerbit"
+        value={penerbit}
+        onChangeText={text => setPenerbit(text)}
+      />
+      
+      <List.Item
+        title="Status"
+        right={props =>
+          <Switch 
+            value={status}
+            onValueChange={(status) => setStatus(status)}
+          />
+        }
+      />
+
+      <Button 
+        icon="content-save" 
+        mode="contained" 
+        onPress={() => onSimpan()}
+        style={{margin:10}}
+      >
+        Simpan
+      </Button>
+    </>
+  );
+}
+
+export default KategoriUbahScreen;
