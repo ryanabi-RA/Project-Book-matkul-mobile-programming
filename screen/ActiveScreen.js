c9831eb3bbff35ed3143f6a4b635afaf738c29ce
ryanabi-RA <ryanabi9@gmail.com>
2022-05-21 02:21:53 +0700

    add ActiveScreen js and NonActiveScreen js

---
+++ screen/ActiveScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Appbar } from 'react-native-paper';
+
+function ActiveScreen({navigation}) {
+  return (
+    <>
+      {/* <Appbar.Header>
+        <Appbar.Content title="Active" />
+      </Appbar.Header>     */}
+    </>
+  );
+}
+
+export default ActiveScreen;
---
+++ screen/NonActiveScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Appbar } from 'react-native-paper';
+
+function NonActiveScreen({navigation}) {
+  return (
+    <>
+      {/* <Appbar.Header>
+        <Appbar.Content title="Non Active" />
+      </Appbar.Header> */}
+    </>
+  );
+}
+
+export default NonActiveScreen;
