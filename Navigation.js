34ce37e4da081ef22f0f33cb24a72b73c870995a
ryannabi-RA <ryanabi9@gmail.com>
2022-06-14 00:37:51 +0700

    Proyek book - mata kuliah mobile programming

--- Navigation.js
+++ Navigation.js
@@ -1,26 +1,38 @@
 import React from 'react';
+import { Appbar } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
-import { MaterialIcons,MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+const TopTab = createMaterialTopTabNavigator();
+
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 const Tab = createMaterialBottomTabNavigator();
 
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import HomeScreen from './screen_tb2/HomeScreen';
-import AccountScreen from './screen_tb2/AccountScreen';
-import HistoryScreen from './screen_tb2/HistoryScreen';
-import BookingScreen from './screen_tb2/BookingScreen';
-import SearchScreen from './screen_tb2/SearchScreen';
-import OrderScreen from './screen_tb2/OrderScreen';
+import HomeScreen from './screen/HomeScreen';
+import KategoriScreen from './screen/KategoriScreen';
+import KategoriTambahScreen from './screen/KategoriTambahScreen';
+import KategoriUbahScreen from './screen/KategoriUbahScreen';
+import SettingScreen from './screen/SettingScreen';
+import BukuScreen from './screen/BukuScreen';
+import ActiveScreen from './screen/ActiveScreen';
+import NonActiveScreen from './screen/NonActiveScreen';
 
 function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:false}}>
-      <Stack.Screen name="HomeScreen" component={NavigationTab} />
-      <Stack.Screen name="SearchScreen" component={SearchScreen} />
-      <Stack.Screen name="OrderScreen" component={OrderScreen} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="HomeScreen" component={NavigationTab} />
+        <Stack.Screen name="ActiveScreen" component={ActiveScreen} />
+        <Stack.Screen name="NonActiveScreen" component={NonActiveScreen} />
+        <Stack.Screen name="BukuScreen" component={BukuScreen} />
+        <Stack.Screen name="KategoriScreen" component={KategoriScreen} />
+        <Stack.Screen name="KategoriTambahScreen" component={KategoriTambahScreen} />
+        <Stack.Screen name="KategoriUbahScreen" component={KategoriUbahScreen} />
+        <Stack.Screen name="SettingScreen" component={SettingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -28,47 +40,51 @@ function Navigation() {
 
 function NavigationTab() {
   return (
-    <Tab.Navigator 
-    activeColor="blue"
-    barStyle={{ 
-      backgroundColor: 'white',
-      height: 80, 
-      justifyContent: "center",
-    }}
-  >
-    <Tab.Screen
+    <Tab.Navigator
+      activeColor="white"
+      barStyle={{ backgroundColor: '#8f9bff' }}
+    >
+      <Tab.Screen
         name="HomeTab"
         component={HomeScreen}
         options={{
           tabBarLabel: 'Home',
-          tabBarIcon: ({ color }) => (<MaterialCommunityIcons  name="home" color={color} size={26} />),
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
         }}
       />
-    <Tab.Screen
-      name="TicketTab"
-      component={BookingScreen}
-      options={{
-        tabBarLabel: 'Tiket',
-        tabBarIcon: ({ color }) => (<MaterialCommunityIcons name="ticket-percent" color={color}size={26} />),
-      }}
-    />
-    <Tab.Screen
-      name="HistoryScreen"
-      component={HistoryScreen}
-      options={{
-        tabBarLabel: 'Riwayat',
-        tabBarIcon: ({ color }) => (<MaterialIcons name="history-edu" size={24} color={color} />),
-      }}
-    />
-    <Tab.Screen
-      name="AkunTab"
-      component={AccountScreen}
-      options={{
-        tabBarLabel: 'Akun',
-        tabBarIcon: ({ color }) => (<MaterialCommunityIcons name="account" color={color} size={26} />),
-      }}
-    /> 
-  </Tab.Navigator>
+      <Tab.Screen
+        name="BookTab"
+        component={BukuScreen}
+        options={{
+          tabBarLabel: 'Book',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="book" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="KategoriTab"
+        component={KategoriScreen}
+        options={{
+          tabBarLabel: 'Kategori Buku',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="folder" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="SettingTab"
+        component={SettingScreen}
+        options={{
+          tabBarLabel: 'Setting',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="cog" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
